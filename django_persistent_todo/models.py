03a3d778916f79bd5ecc98370e5d13efd074821c
InternetFett <internetfett@gmail.com>
2013-08-30 00:07:29 -0500

    allow null

--- django_persistent_todo/models.py
+++ django_persistent_todo/models.py
@@ -5,7 +5,7 @@ class ToDoItem(models.Model):
     """ Items are associated with one another by list id. """
     list_id = models.IntegerField(db_index=True, blank=False)
     text = models.CharField(max_length=255, blank=False)
-    order = models.IntegerField(blank=True)
+    order = models.IntegerField(blank=True, null=True)
     complete = models.BooleanField()
 
     def __unicode__(self):
