ac7498259a80c3bb5e7fd49bae24d8ce81f5fed8
InternetFett <internetfett@gmail.com>
2013-08-30 00:08:23 -0500

    whitespace

--- example/admin.py
+++ example/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from django_persistent_todo.models import ToDoItem
 
+
 class ToDoItemAdmin(admin.ModelAdmin):
     """ Register item admin for demonstration. """
     list_display = ('list_id', 'text')
