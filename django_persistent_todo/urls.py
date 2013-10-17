1a9a890c6f3c1c211fbde919612561a5b7e65dea
InternetFett <internetfett@gmail.com>
2013-10-17 00:30:51 -0500

    add delete functionality

--- django_persistent_todo/urls.py
+++ django_persistent_todo/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
-from django_persistent_todo.views import AddToDoView
+from django_persistent_todo.views import AddToDoView, DelToDoView
 
 urlpatterns = patterns('',
     url(r'^add/$', AddToDoView.as_view(), name='add'),
+    url(r'^delete/(?P<pk>\d+)$', DelToDoView.as_view(), name='delete'),
 )
--- django_persistent_todo/views.py
+++ django_persistent_todo/views.py
@@ -1,10 +1,18 @@
-from django.views.generic import FormView
+from django.http import HttpResponseRedirect
+from django.views.generic import FormView, TemplateView
+from django.views.generic.detail import SingleObjectMixin
 
 from django_persistent_todo.forms import AddToDoForm
 from django_persistent_todo.models import ToDoItem
 
 
-class AddToDoView(FormView):
+class RedirectMixin(object):
+    def get_success_url(self):
+        """ Redirect to the origin. """
+        return self.request.META.get('HTTP_REFERER','/')
+
+
+class AddToDoView(RedirectMixin, FormView):
     form_class = AddToDoForm
     http_method_names = ['post']
     
@@ -16,7 +24,14 @@ class AddToDoView(FormView):
         item.list_id = form.cleaned_data.get('list_id')
         item.save()
         return super(AddToDoView, self).form_valid(form)
-        
-    def get_success_url(self):
-        """ Redirect to the origin. """
-        return self.request.META.get('HTTP_REFERER','/')
+
+
+class DelToDoView(RedirectMixin, TemplateView, SingleObjectMixin):
+    http_method_names = ['get']
+    model = ToDoItem
+    
+    def get(self, request, *args, **kwargs):
+        """ Delete existing todo item. """
+        if self.get_object():
+            self.get_object().delete()
+        return HttpResponseRedirect(self.get_success_url())
